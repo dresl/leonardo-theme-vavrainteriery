70fad66449e0a2b302a72cd7fbcf8ae9af8835c3
Dominik Resl <nikicresl@gmail.com>
2017-04-02 14:49:04 +0200

    clean up

--- leonardo_theme_vavrainteriery/__init__.py
+++ leonardo_theme_vavrainteriery/__init__.py
@@ -4,6 +4,8 @@ LEONARDO_APPS = [
     'leonardo_module_analytics'
 ]
 
-LEONARDO_JS_FILES = []
+LEONARDO_JS_FILES = [
+    'js/vavra_interiery.js'
+]
 LEONARDO_SCSS_FILES = []
 LEONARDO_CSS_FILES = []
