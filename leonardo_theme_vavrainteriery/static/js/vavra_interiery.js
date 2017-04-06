ff69b0d179df7c64a82421bb7d575d751f221143
Dominik Resl <nikicresl@gmail.com>
2017-04-06 21:28:58 +0200

    styling

--- leonardo_theme_vavrainteriery/static/js/vavra_interiery.js
+++ leonardo_theme_vavrainteriery/static/js/vavra_interiery.js
@@ -4,9 +4,16 @@
 $(function() {
 setTimeout(function(){
         var footerHeight = $('footer').height() + 15;
+        var heightHlaf = window.innerHeight/2;
+        var insertSizeBg = heightHlaf + 'px, cover';
+        $('.slide2').css('background-position', '100px bottom,center')
+        $('.slide2').css('background-size', insertSizeBg)
         $('.leonardo-widget.contextnavigation-base-default').css('height',window.innerHeight - footerHeight + 'px');
         window.onresize = function(event) {
             $('.leonardo-widget.contextnavigation-base-default').css('height',window.innerHeight - footerHeight + 'px');
+            var heightHlaf = window.innerHeight/2;
+            var insertSizeBg = heightHlaf + 'px, cover';
+            $('.slide2').css('background-size', insertSizeBg)
         };
     }, 300);
 });
