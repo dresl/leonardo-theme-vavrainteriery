67ea041dc6e94de820337b4f508ab1210d2dabe7
Dominik Resl <nikicresl@gmail.com>
2017-04-06 19:59:28 +0200

    styling and fix bugs

--- leonardo_theme_vavrainteriery/static/js/vavra_interiery.js
+++ leonardo_theme_vavrainteriery/static/js/vavra_interiery.js
@@ -1,10 +1,12 @@
 /* JS */
 
 // set height to fullscreen backgrounds
-$(function() {    
-    var footerHeight = $('footer').height() + 15;
-    $('.leonardo-widget.contextnavigation-base-default').css('height',window.innerHeight - footerHeight + 'px');
-    window.onresize = function(event) {
+$(function() {
+setTimeout(function(){
+        var footerHeight = $('footer').height() + 15;
         $('.leonardo-widget.contextnavigation-base-default').css('height',window.innerHeight - footerHeight + 'px');
-    };
+        window.onresize = function(event) {
+            $('.leonardo-widget.contextnavigation-base-default').css('height',window.innerHeight - footerHeight + 'px');
+        };
+    }, 300);
 });
