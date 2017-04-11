81df65b4c450878b36dce1e2b0f73c2b62ab0dc8
Dominik Resl <nikicresl@gmail.com>
2017-04-11 22:24:16 +0200

    styling

--- leonardo_theme_vavrainteriery/__init__.py
+++ leonardo_theme_vavrainteriery/__init__.py
@@ -5,7 +5,13 @@ LEONARDO_APPS = [
 ]
 
 LEONARDO_JS_FILES = [
-    'js/vavra_interiery.js'
+    'js/vavra_interiery.js',
+    'js/photoswipe.min.js',
+    'js/photoswipe-ui-default.min.js',
+    'js/photoswipe-multigalleries.js',
 ]
 LEONARDO_SCSS_FILES = []
-LEONARDO_CSS_FILES = []
+LEONARDO_CSS_FILES = [
+    'css/default-skin.min.css',
+    'css/photoswipe.min.css',
+]
